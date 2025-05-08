59ea0c8ba68b30b07bfa61101619072172441c4f
yash.patel1 <yash.patel@drcsystems.com>
2025-05-08 14:59:19 -0400

    Change in logger.py

--- src/logger.py
+++ src/logger.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 LOG_DIRS = os.path.join(os.getcwd(), "logs")
 LOG_FILE = os.path.join(
-    LOG_DIRS, f"log_{datetime.now().strftime("%y-%m-%d")}.log")
+    LOG_DIRS, f"log_{datetime.now().strftime('%y-%m-%d')}.log")
 
 logging.basicConfig(filename=LOG_FILE,
                     format="%(asctime)s - %(levelname)s - %(message)s",
